6d6614e38630dc674fb6b07fc6c98a2261a45f81
maneeshpm <manu.pm55@gmail.com>
2020-05-12 10:58:49 +0530

    changes applied

--- Assignment2/signer.py
+++ Assignment2/signer.py
@@ -1,13 +1,13 @@
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
-from Crypto.Signature import pss
+from Crypto.Signature import PKCS1_PSS
 
 plainText = input("Enter encryption text: ")
 keyPath = input("Enter the path of the private key: ")
 
 key = RSA.import_key(open(keyPath).read())
 h = SHA256.new(plainText.encode('utf-8'))
-sign = pss.new(key).sign(h)
+sign = PKCS1_PSS.new(key).sign(h)
 
 print(sign.hex())
 
--- Assignment2/varifier.py
+++ Assignment2/varifier.py
@@ -1,6 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
-from Crypto.Signature import pss
+from Crypto.Signature import PKCS1_PSS
 
 keyPath = input("Enter the public key: ")
 signHex = str(input("Enter the encrypted text: "))
@@ -8,10 +8,10 @@ plainText = input("Enter the unencrypted text: ")
 
 publicKey = RSA.import_key(open(keyPath).read())
 h = SHA256.new(plainText.encode('utf-8'))
-varifier = pss.new(publicKey)
+varifier = PKCS1_PSS.new(publicKey)
 
-try:
-    varifier.verify(h, bytes.fromhex(signHex))
+
+if varifier.verify(h,bytes.fromhex(signHex)):
     print("\nSignature valid!")
-except(ValueError, TypeError):
+else:
     print("\nValidation failed!")
