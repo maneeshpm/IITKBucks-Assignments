d17b5845b6fd7947c52f265f4d65f441b7835578
maneeshpm <manu.pm55@gmail.com>
2020-05-15 14:11:25 +0530

    assignment4 complete

---
+++ Assignment4/classes.py
@@ -0,0 +1,72 @@
+import hashlib
+
+class Inp:
+    def __init__(self, txnID, opIndex, sign):
+        self.txnID = txnID
+        self.opIndex = opIndex
+        self.sign = sign
+        self.lenSign = len(self.sign)
+
+    def getBinaryInput(self):
+        return ((bytes.fromhex(self.txnID))
+        +(self.opIndex.to_bytes(4, byteorder = 'big'))
+        +(self.lenSign.to_bytes(4, byteorder = 'big'))
+        +(self.sign))
+    
+    def __str__(self):
+        pr = ("\t\tTransaction ID: {}\n".format(self.txnID)
+        +"\t\tIndex: {}\n".format(self.opIndex)
+        +"\t\tLength of signature: {}\n".format(self.lenSign)
+        +"\t\tSignature: {}\n".format(self.sign.hex()))
+        return pr          
+
+class Output:
+    def __init__(self, noCoins, pubKey):
+        self.noCoins = noCoins
+        self.pubKey = pubKey
+        self. lenPubKey = len(self.pubKey)
+
+    def getBinaryOutput(self):
+        return (self.noCoins.to_bytes(8, byteorder = 'big')
+        +self.lenPubKey.to_bytes(4,byteorder = 'big')
+        +self.pubKey)
+
+    def __str__(self):
+        pr = ("\t\tNumber of coins: {}\n".format(self.noCoins)
+        +"\t\tLength of public key: {}\n".format(self.lenPubKey)
+        +"\t\tPublic key: {}\n".format(self.pubKey))
+        return pr
+
+class Txn:
+    totInput=[]
+    totOutput=[]
+    def _init_(self):
+        self.noInputs = 0
+        self.noOutputs = 0
+        self.totInput = []
+        self.totOutput = []
+
+    def getTxnData(self):
+        data = self.noInputs.to_bytes(4, byteorder = 'big')
+        for inps in self.totInput:
+            data += inps.getBinaryInput()
+        data += (self.noOutputs.to_bytes(4, byteorder = 'big'))
+        for ops in self.totOutput:
+            data += (ops.getBinaryOutput())
+        return data
+
+    def getTxnHash(self):
+        h = hashlib.sha256()
+        h.update(self.getTxnData())
+        return h.hexdigest()
+    
+    def getTxnDetails(self):
+        print("Transaction ID: {}\n".format(self.getTxnHash()))
+        print("\nNumber of inputs: {}\n".format(self.noInputs))
+        for i in range(self.noInputs):
+            print("\tInput #{}:\n".format(i+1))
+            print(self.totInput[i])
+        print("\nNumber of outputs: {}\n".format(self.noOutputs))
+        for i in range(self.noOutputs):
+            print("\tOutput #{}:\n".format(i+1))
+            print(self.totOutput[i])
